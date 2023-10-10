77d2d5f25c1b7cd53fd21c58f8676ae2cdce05c8
daniel-js0 <danielolagbegi1@gmail.com>
2023-10-10 16:59:56 +0100

    first commit

---
+++ app/about.js
@@ -0,0 +1,11 @@
+const About = () => {
+    return (
+        <div>
+            <h1 className='font-bold'>About</h1>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Numquam asperiores deserunt est!</p>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Numquam asperiores deserunt est!</p>
+        </div>
+      );
+}
+ 
+export default About;
